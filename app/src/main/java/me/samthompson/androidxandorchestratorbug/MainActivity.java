561148877050c3fc0d6f7bf9a4796fa56dd7ce4b
Sam Thompson <samuel.thompson@robinhood.com>
2018-09-13 13:24:16 -0700

    Pre-androidx code

--- app/src/main/java/me/samthompson/androidxandorchestratorbug/MainActivity.java
+++ app/src/main/java/me/samthompson/androidxandorchestratorbug/MainActivity.java
@@ -1,8 +1,8 @@
 package me.samthompson.androidxandorchestratorbug;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
